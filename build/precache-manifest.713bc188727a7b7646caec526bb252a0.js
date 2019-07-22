8a5ef2981049197a342bfd4d58186ec4b593c0d7
shuang <shuangxiao@gmail.com>
2019-07-22 16:56:56 -0400

    add monters rolodex
    commit

---
+++ build/precache-manifest.713bc188727a7b7646caec526bb252a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3bdf9ef2d54801b8b06450beb9e7282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f447287b999cdd14d1e4",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f447287b999cdd14d1e4",
+    "url": "/static/js/main.dfa329a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
