897c8f72602e2697f4442f3f3cb805317947aede
shuang <shuangxiao@gmail.com>
2019-07-22 16:58:10 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ render(){
     );
     return ( 
     <div className='App'>
-      <h1> Monsters Rolodex </h1>
+      <h1> My Monsters Rolodex </h1>
      <SearchBox 
      placeholder = 'search monsters' 
      handleChange = {this.handleChange}
